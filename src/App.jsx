d02d8ac16da3e54b639747701a61249b33aaf735
gyullee96 <gyullee96@naver.com>
2025-04-13 16:55:26 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import { Grid, TextField } from '@mui/material'
+import ContactForm from './components/ContactForm'
+import ContactList from './components/ContactList'
+
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Phone Book</h1>
+      <Grid container spacing={2}>
+        <Grid size={6}>
+          <ContactForm />
+        </Grid>
+        <Grid size={6}>
+          <ContactList />
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default App
